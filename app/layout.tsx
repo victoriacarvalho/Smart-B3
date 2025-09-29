4277fc2bf0f754d6b6a51b84194921fa92b65665
victoria carvalho <victoriacostasilvac@gmail.com>
2025-09-29 14:43:21 -0300

    feat: add cleark button and clerk themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export const metadata: Metadata = {
   title: "Smart B3",
@@ -17,7 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="dark">
-        <ClerkProvider> {children}</ClerkProvider>
+        <ClerkProvider
+          appearance={{
+            baseTheme: dark,
+          }}
+        >
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { dark } from "@clerk/themes";
 
-const Home = () => {
-  return <Button> teste</Button>;
+const Home = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("\login");
+  }
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Home;
