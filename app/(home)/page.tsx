9ff2968e288176b22cd87ff4e693f3d545a32cea
victoriacarvalho <victoriacostasilvac@gmail.com>
2025-10-17 13:58:23 -0300

    fix: remove ununsed components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,7 +9,6 @@ const DashboardPage = async () => {
       <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex items-center justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
-          <AiReportButton />
           <AddTransactionDialog />
           <div className="flex items-center gap-4"></div>
         </div>
