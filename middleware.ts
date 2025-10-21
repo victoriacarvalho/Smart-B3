69728fd767e45d42f22836b22e8c4bc6b5bab76e
victoriacarvalho <victoriacostasilvac@gmail.com>
2025-10-21 14:08:51 -0300

    fix: now the cron call is not blocked by the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/login(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/login(.*)",
+  "/api/cron/send-notifications",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
