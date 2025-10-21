65280f2548b54c16b552320f8ebfbca5d3583e80
victoriacarvalho <victoriacostasilvac@gmail.com>
2025-10-21 13:39:47 -0300

    fix: add toaster notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "@/app/_components/ui/sonner"; // Importa o Toaster
 
 export const metadata: Metadata = {
   title: "Smart B3",
@@ -22,6 +23,7 @@ export default function RootLayout({
           }}
         >
           {children}
+          <Toaster richColors />{" "}
         </ClerkProvider>
       </body>
     </html>
