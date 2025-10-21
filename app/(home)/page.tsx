e74f8135c127ed148b78c30e1a2dd7d0a832c240
victoriacarvalho <victoriacostasilvac@gmail.com>
2025-10-21 14:18:19 -0300

    feat: create time select component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { AddTransactionDialog } from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import TimeSelect from "./_components/time-select";
 
 const DashboardPage = async () => {
   return (
@@ -8,8 +9,11 @@ const DashboardPage = async () => {
       <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex items-center justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
-          <AddTransactionDialog />
-          <div className="flex items-center gap-4"></div>
+
+          <div className="flex items-center gap-4">
+            <AddTransactionDialog />
+            <TimeSelect />
+          </div>
         </div>
       </div>
     </>
